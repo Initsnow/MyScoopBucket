f33ff576546e4512dfaafedcbcc80e3aaef84e8b
Initsnow <79002121+Initsnow@users.noreply.github.com>
2024-05-10 10:33:32 +0800

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "Initsnow/MyScoopBucket:main"
+    [String]$upstream = "Initsnow/MyScoopBucket:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
